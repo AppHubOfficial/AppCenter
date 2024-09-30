ec1ea7cdea39ffd3dea801077d5486dac83823e3
simone <AppHub2024@gmail.com>
2024-09-30 09:48:26 +0200

    Aggiunto logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 
+import Header from "./components/Header";
+
 function App() {
   const [apps, setApps] = useState([
     { id: 1, name: 'App 1', description: 'This is a description for App 1.' },
@@ -10,7 +12,7 @@ function App() {
 
   return (
     <div>
-      
+      <Header></Header>
       
     </div>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import '../assets/styles/Header.css';
+import AppCenterLogo from '../assets/images/AppCenterLogo.jsx';
 
 const Header = () => {
     return (
         <header>
-            
+            <AppCenterLogo></AppCenterLogo>
         </header>
     );
 };
